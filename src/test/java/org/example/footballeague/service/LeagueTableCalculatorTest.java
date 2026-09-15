8a27c11452a09e2e12c87d54519c4acb836f20e7
Jaydin Morrison <4260354@myuwc.ac.za>
2026-09-15 14:26:23 +0200

    unit test if home team wins

---
+++ src/test/java/org/example/footballeague/service/LeagueTableCalculatorTest.java
@@ -0,0 +1,42 @@
+package org.example.footballeague.service;
+
+import org.example.footballeague.domain.MatchResult;
+import org.example.footballeague.domain.TeamStanding;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class LeagueTableCalculatorTest {
+
+    @Test
+    void homeTeamWins(){
+        MatchResult match = new MatchResult();
+        match.homeTeam = "Manchester City";
+        match.awayTeam = "Manchester United";
+        match.homeGoals = 2;
+        match.awayGoals = 1;
+
+        LeagueTableCalculator calculator = new LeagueTableCalculator();
+        List<TeamStanding> standings = calculator.calculateLeagueTable(Collections.singletonList(match));
+
+        TeamStanding manCity = standings.stream()
+                .filter(team -> team.teamName.equals("Manchester City"))
+                .findFirst()
+                .orElseThrow();
+        TeamStanding manUtd = standings.stream()
+                .filter(team -> team.teamName.equals("Manchester United"))
+                .findFirst()
+                        .orElseThrow();
+        assertEquals(2, manCity.points);
+        assertEquals(1, manUtd.points);
+        assertEquals(1, manCity.won);
+        assertEquals(1, manUtd.lost);
+        assertEquals(1, manCity.draw);
+        assertEquals(0, manUtd.draw);
+        assertEquals(2, manCity.goalsFor);
+        assertEquals(1, manUtd.goalsAgainst);
+
+    }
+}
