76c50d33fe9c930b4de561b08388e12e12dc1e64
Jaydin Morrison <4260354@myuwc.ac.za>
2026-09-15 13:30:03 +0200

    initial commit

---
+++ src/main/java/org/example/footballeague/FootbalLeagueApplication.java
@@ -0,0 +1,13 @@
+package org.example.footballeague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootbalLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootbalLeagueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/footballeague/FootbalLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.footballeague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootbalLeagueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
